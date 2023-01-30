0b8d503fc57ae7cd54f7bb969aa2200f0542db59
Imnotndesh <slateinbox@gmail.com>
2023-01-29 23:08:33 -0800

    added multiple page scraping

--- jumiaScraper.py
+++ jumiaScraper.py
@@ -2,21 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import sys
+import json
 
-def anim():
-    animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
-    for i in range(len(animation)):
-        time.sleep(0.2)
-        sys.stdout.write("\r" + animation[i % len(animation)])
-        sys.stdout.flush()
-    print("\n")
+class animations:
+    def itemAnim(self,subSec):
+        animation = [(f"Finding {subSec}."),(f"Finding {subSec}.."),(f"Finding {subSec}..."),(f"Finding {subSec}...."),(f"Finding {subSec}.....")]
+        for i in range(len(animation)):
+            time.sleep(0.2)
+            sys.stdout.write("\r" + animation[i % len(animation)])
+            sys.stdout.flush()
+        print("\n")
+
+    def innAnim(self):
+        animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+        for i in range(len(animation)):
+            time.sleep(0.2)
+            sys.stdout.write("\r" + animation[i % len(animation)])
+            sys.stdout.flush()
+        print("\n")
+
+z = animations()
+
 
 class Fetcher:
-    print("***Jumia Section***")
-    def jumiaFetcher(self,subSec):
-        anim()
+    def jumiaFetcher(self,subSec,pageAmt):
+        z.innAnim()
         totpage = 0
-        while totpage <5:
+        while totpage <pageAmt:
             totpage = totpage + 1
             url = (f"https://www.jumia.co.ke/catalog/?q={subSec}&sort=lowest-price&shipped_from=country_local&page={totpage}")
             site = requests.get(url)
@@ -24,19 +36,36 @@ class Fetcher:
             soup = BeautifulSoup(site.text,'html.parser')
             products = soup.find_all(class_='core')
             for product in products:
-                imageLink = product.find(class_="img")
                 itemName = product.find(class_='name')
                 itemPrice = product.find(class_='prc')
 
-                print(f"Product : {itemName.get_text()}")
-                print(f" Discounted price : {itemPrice.get_text()}")
-                print (f"Url : https://www.jumia.co.ke{product['href']}")
+                finalName = (f"Product : {itemName.get_text()}")
+                finalPrice = (f" Discounted price : {itemPrice.get_text()}")
+                finalLink = (f"Url : https://www.jumia.co.ke{product['href']}")
+                print(finalName)
+                print(finalPrice)
+                print(finalLink)
                 print("*************************************")
                 print(" ")
+
+
             
-
-
-subSec = input("Search for product...... ")
-
 x=Fetcher()
-x.jumiaFetcher(subSec)
+program = 0
+
+while program != 1:
+    print("|||| Welcome to the Jumia Kenya scraper ||||")
+    subSec = input("Search for a product (Type 'quit' to exit the program)...... ")
+
+    if subSec == "quit":
+        program = 1
+
+
+    if subSec != "quit":
+        pageAmt = int(input("Fetch how many pages? "))
+        z.itemAnim(subSec)
+        x.jumiaFetcher(subSec,pageAmt)
+
+else:
+    print("Quitting...")
+
